b20f60288a2af1dfe784cb3f1d2ac668e11bb82c
capnchill <subodhchilwal123@gmail.com>
2024-04-11 20:36:59 +0530

    Added transitions to the component switch, component delete

--- src/routes/components/Polllist.svelte
+++ src/routes/components/Polllist.svelte
@@ -1,11 +1,13 @@
 <script>
+	import { fade, scale } from 'svelte/transition';
 	import PollStore from '../../stores/Pollstore';
 	import PollCard from './PollCard.svelte';
+	import { flip } from 'svelte/animate';
 </script>
 
-<div class="my-4 grid grid-cols-2 gap-4">
+<div class="my-4 grid grid-cols-2 gap-4" in:fade>
 	{#each $PollStore as poll (poll.id)}
-		<div class="w-[450px]">
+		<div class="w-[450px]" out:scale|local animate:flip={{ duration: 500 }}>
 			<PollCard {poll} on:deletePoll on:addtoVoteA on:addtoVoteB></PollCard>
 		</div>
 	{:else}
